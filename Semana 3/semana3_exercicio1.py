e827dc23eef62655bcd736d6c0aab0e5a42c24a7
Angela-Colombo <a.souzacolombo@gmail.com>
2024-04-15 18:27:02 -0300

    Primeiro commit

---
+++ Semana 3/semana3_exercicio1.py
@@ -0,0 +1,13 @@
+# EXERCÍCIO 1
+
+# Atividade: Fazer macarrão
+    # Passo 1: Escolher/comprar o tipo de macarrão a ser cozinhado
+    # Passo 2: Escolher o molho e acompanhamentos do macarrão 
+    # Passo 3: Colocar a água na panela 
+    # Passo 4: Colocar a panela no fogo e esperar ferver
+    # Passo 5: Quando ferver, colocar o macarrão e deixar até o tempo de cozimento indicado na embalagem
+    # Passo 6: Após o tempo de cozimento, escorrer a água quente do macarrão e reservar. 
+    # Passo 7: Preparar o molho e acompanhamentos do macarrão. 
+    # Passo 8: Juntar o macarrão com o molho e acompanhamentos. 
+    # Passo 9: Colocar a porção desejada do macarrão pronto no prato. 
+    # Passo 10: Se deliciar e ser feliz. 
---
+++ Semana 3/semana3_exercicio2.py
@@ -0,0 +1,28 @@
+produto1_0050 = 5
+produto2_0054 = 10
+
+selecao_produto1 = int(input("Quantos produtos 1 você quer adicionar ao carrinho? "))
+selecao_produto2 = int(input("Quantos produtos 2 você quer adicionar ao carrinho? "))
+
+valor_produto1 = produto1_0050*selecao_produto1
+valor_produto2 = produto2_0054*selecao_produto2
+
+if selecao_produto1 == 1 and selecao_produto2 == 0:
+        print("O valor da sua compra é R$ ", produto1_0050, ",00. Que tal adicionar mais um produto para ganhar descontos? :) ", sep = "")
+
+elif selecao_produto1 == 0 and selecao_produto2 == 1:
+         print("O valor da sua compra é R$ ", produto2_0054, ",00. Que tal adicionar mais um produto para ganhar descontos? :) ", sep = "")
+
+elif selecao_produto1 == 2 and selecao_produto2 == 0:
+        print("Maravilha! Você ganhou R$ 5,00 de desconto! O valor da sua compra é R$ ", (valor_produto1)-5, ",00.", sep = "")
+
+elif selecao_produto1 == 0 and selecao_produto2 == 2:
+        print("Maravilha! Você ganhou R$ 5,00 de desconto! O valor da sua compra é R$ ", (valor_produto2)-5, ",00.", sep = "")
+
+elif selecao_produto1 == 1 and selecao_produto2 == 1:
+        print("Uhul! Você ganhou 50% de desconto! :D. O valor da sua compra é R$", (valor_produto1+valor_produto2)/2)
+
+
+
+
+
